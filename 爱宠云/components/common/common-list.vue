1fb3f1ddffef92f038eda37d79f4bf426801eab5
maysky-blue <1768359097@qq.com>
2020-06-08 18:58:46 +0800

    完善首页帖子列表排布和效果

--- 爱宠云/components/common/common-list.vue
+++ 爱宠云/components/common/common-list.vue
@@ -1,96 +1,183 @@
 <template>
-	<!--列表样式-->
-	<view class="p-2">
-		<!--头像昵称|关注按钮-->
+	<view class="p-2 animated fast fadeIn">
+		<!-- 头像昵称 | 关注按钮 -->
 		<view class="flex align-center justify-between">
 			<view class="flex align-center">
-				<!--头像-->
+				<!-- 头像 -->
 				<image class="rounded-circle mr-2" 
-				:src="item.userpic" @click="openSpace"
+				:src="item.userpic" @click="openSpace(item.user_id)"
 				style="width: 65rpx;height: 65rpx;" 
 				lazy-load></image>
-				<!--昵称及发布时间-->
+				<!-- 昵称发布时间 -->
 				<view>
-					<view class="font" style="line-height: 1.5;">{{item.username}}</view>
-					<text class="font-sm text-light-muted" style="line-height: 1.5;">{{item.newstime}}</text>
+					<view class="font" style="line-height: 1.5;">
+						{{item.username}}
+					</view>
+					<text class="font-sm text-light-muted" 
+					style="line-height: 1.5;">
+						{{item.newstime|formatTime}}
+					</text>
 				</view>
 			</view>
-			<!--按钮-->
-			<view @click="follow" v-if="!item.isFollow"
+			<!-- 按钮 -->
+			<view @click="follow" 
+			v-if="!item.isFollow && user.id !== item.user_id"
 			class="flex align-center justify-center rounded bg-main text-white animated faster" 
 			style="width: 90rpx;height: 50rpx;"
-			hover-class="rubberBand">
+			hover-class="jello"
+			>
 				关注
 			</view>
 		</view>
-		<!--标题-->
+		<!-- 标题 -->
 		<view class="font-md my-1" @click="openDetail">{{item.title}}</view>
-		<!--图片-->
-		<image v-if="item.titlepic" :src="item.titlepic" @click="openDetail"
-		class="rounded w-100" style="height: 350rpx;"></image>
-		<!--图标按钮-->
+		<!-- 帖子详情 -->
+		<slot>
+			<!-- 图片 -->
+			<image v-if="item.titlepic" :src="item.titlepic" @click="openDetail" 
+			style="height: 350rpx;" class="rounded w-100" mode="aspectFill"></image>
+		</slot>
+		<!-- 图标按钮 -->
 		<view class="flex align-center">
-			<!--赞-->
-			<view class="flex align-center justify-center flex-1 animated faster" hover-class="rubberBand text-main"
-			@click="doSupport('support')"
-			:class="item.support.type === 'support' ? 'support-active' : '' ">
-				<!--因为animate.css是阉割版，所以并没有全部动画效果，故选了rubberBand-->
-				<text class="iconfont icon-dianzan2 mr-2"></text><!--这个点赞2，如果图标库改了，这里图标名icon-dianzan2也要记得更改-->
+			<!-- 顶 -->
+			<view class="flex align-center justify-center flex-1 animated faster"
+			hover-class="jello text-main" @click="doSupport('support')"
+			:class="item.support.type === 'support' ? 'support-active' : ''">
+				<text class="iconfont icon-dianzan2 mr-2"></text>
 				<text>{{item.support.support_count > 0 ? item.support.support_count : '支持'}}</text>
 			</view>
-			<!--踩-->
-			<view class="flex align-center justify-center flex-1 animated faster" hover-class="rubberBand text-main" 
-			@click="doSupport('unsupport')"
-			:class="item.support.type === 'unsupport' ? 'support-active' : '' ">
-				<text class="iconfont icon-cai mr-2"></text><!--也要记得更改-->
+			<!-- 踩 -->
+			<view class="flex align-center justify-center flex-1 animated faster"
+			hover-class="jello text-main" @click="doSupport('unsupport')"
+			:class="item.support.type === 'unsupport' ? 'support-active' : ''">
+				<text class="iconfont icon-cai mr-2"></text>
 				<text>{{item.support.unsupport_count > 0 ? item.support.unsupport_count : '反对'}}</text>
 			</view>
-			<!--评论-->
-			<view class="flex align-center justify-center flex-1 animated faster" hover-class="rubberBand text-main"
-			@click="openDetail">
-				<text class="iconfont icon-pinglun mr-2"></text><!--也要记得更改-->
+			<view class="flex align-center justify-center flex-1 animated faster"
+			hover-class="jello text-main" @click="doComment">
+				<text class="iconfont icon-pinglun2 mr-2"></text>
 				<text>{{item.comment_count > 0 ? item.comment_count : '评论'}}</text>
 			</view>
-			<!--分享-->
-			<view class="flex align-center justify-center flex-1 animated faster" hover-class="rubberBand text-main"
-			@click="openDetail">
-				<text class="iconfont icon-fenxiang mr-2"></text><!--也要记得更改-->
+			<view class="flex align-center justify-center flex-1 animated faster"
+			hover-class="jello text-main" @click="doShare">
+				<text class="iconfont icon-fenxiang mr-2"></text>
 				<text>{{item.share_num > 0 ? item.share_num : '分享'}}</text>
 			</view>
 		</view>
 	</view>
-	
 </template>
 
 <script>
-	export default{
-		props:{
-			item:Object,
-			index:Number
-		},
-		methods:{
-			//打开个人空间
-			openSpace(){
-				console.log('打开个人空间');
+	import $T from '@/common/time.js';
+	import {mapState} from 'vuex';
+	export default {
+		props: {
+			item: Object,
+			index:{
+				type:Number,
+				default:-1
 			},
-			//关注
-			follow(){
-				//通知父组件
-				this.$emit('follow',this.index)
-			},
-			//进入详情页
-			openDetail(){
-				console.log('进入详情页');
-			},
-			//顶踩操作
-			doSupport(type){
-				this.$emit('doSupport',{
-					type:type,
-					index:this.index
-				})
+			isdetail:{
+				type:Boolean,
+				default:false
 			}
 		},
-		
+		filters: {
+			formatTime(value) {
+				return $T.gettime(value);
+			}
+		},
+		computed: {
+			...mapState({
+				user:state=>state.user
+			})
+		},
+		methods: {
+			// 打开个人空间
+			openSpace(user_id) {
+				uni.navigateTo({
+					url: '/pages/user-space/user-space?user_id='+user_id,
+				});
+			},
+			// 关注
+			follow(){
+				this.checkAuth(()=>{
+					this.$H.post('/follow',{
+						follow_id:this.item.user_id
+					},{
+						token:true
+					}).then(res=>{
+						// 通知更新
+						uni.$emit('updateFollowOrSupport',{
+							type:"follow",
+							data:{
+								user_id:this.item.user_id
+							}
+						})
+					})
+				})
+			},
+			// 进入详情页
+			openDetail(){
+				// 处于详情中
+				if (this.isdetail) return;
+				uni.navigateTo({
+					url: '../../pages/detail/detail?detail='+JSON.stringify(this.item),
+				});
+				// 加入历史记录
+				let list =  uni.getStorageSync('history')
+				list = list ? JSON.parse(list) : []
+				let index = list.findIndex(v=>v.id === this.item.id)
+				if(index === -1){
+					list.unshift(this.item)
+					uni.setStorageSync('history',JSON.stringify(list))
+				}
+			},
+			// 顶踩操作
+			doSupport(type){
+				this.checkAuth(()=>{
+					this.$H.post('/support',{
+						post_id:this.item.id,
+						type:type === 'support' ? 0 : 1
+					},{
+						token:true,
+						native:true
+					}).then(res=>{
+						if(res.data.errorCode){
+							return uni.showToast({
+								title:res.data.msg,
+								icon: 'none'
+							});
+						}
+						console.log('通知父组件');
+						// 通知父组件
+						uni.$emit('updateFollowOrSupport',{
+							type:"support",
+							data:{
+								type:type,
+								id:this.item.id
+							}
+						})
+					})
+				})
+			},
+			// 评论
+			doComment(){
+				this.checkAuth(()=>{
+					if (!this.isdetail) {
+						return this.openDetail()
+					}
+					this.$emit('doComment')
+				})
+			},
+			// 分享
+			doShare(){
+				if (!this.isdetail) {
+					return this.openDetail()
+				}
+				this.$emit('doShare')
+			}
+		},
 	}
 </script>
 
