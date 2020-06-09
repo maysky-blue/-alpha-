e4bd57a8d5a662b0a52d1d4b2d9e6bbb4006fd74
maysky-blue <1768359097@qq.com>
2020-06-09 20:18:14 +0800

    判断是否是登入用户本人
    判断是否是登入用户本人

--- 爱宠云/components/user-chat/user-chat-list.vue
+++ 爱宠云/components/user-chat/user-chat-list.vue
@@ -1,32 +1,43 @@
 <template>
-	<!-- 左边-->
-	<view class="flex align-start my-2"
-	:style="item.user_id === 1 ? 'flex-direction: row-reverse;' : ''">
-		<image :src="item.avatar"
-		 style="width: 100rpx;height: 100rpx;"
-		 class="rounded-circle"></image>
-		<view class="bg-light p-2 rounded mx-2"
-		style="max-width: 400rpx;min-width: 100rpx;">
-		{{item.data}}
+	<view>
+		<!-- 时间 -->
+		<view v-if="shortTime"
+		class="py-2 flex align-center justify-center font-sm text-light-muted">
+			{{shortTime}}
+		</view>
+		<!-- 消息气泡 -->
+		<view class="flex align-start px-2 my-2"
+		:style="isSelf ? 'flex-direction: row-reverse;' : ''">
+			<image :src="item.avatar ? item.avatar : '/static/default.jpg'"
+			style="width: 100rpx;height: 100rpx;"
+			class="rounded-circle bg-secondary"></image>
+			<view class="bg-light p-2 rounded mx-2"
+			style="max-width: 400rpx;">
+				{{item.data}}
+			</view>
 		</view>
 	</view>
-
 </template>
 
 <script>
-	//模拟当前用户的id
-	const uid = 1;
+	import $T from '@/common/time.js';
 	export default {
 		props:{
 			item:Object,
-			index:Number
+			index:Number,
+			pretime:[Number,String]
 		},
-		computed:{
-			//是否是用户本人
-			isSelf(){
-				return uid === this.item.user_id
+		computed: {
+			// 是否是登录用户本人
+			isSelf() {
+				let myId = this.$store.state.user.id
+				return myId === this.item.user_id
+			},
+			// 转化时间
+			shortTime(){
+				return $T.getChatTime(this.item.create_time,this.pretime)
 			}
-		}
+		},
 	}
 </script>
 
--- 爱宠云/components/user-list/user-list.vue
+++ 爱宠云/components/user-list/user-list.vue
@@ -1,28 +1,41 @@
 <template>
-	<view class="p-2 flex align-center
-	 border-bottom border-light-secondary" hover-class="bg-light">
-		<image :src="item.avatar" 
+	<view class="p-2 flex align-center border-bottom border-light-secondary" hover-class="bg-light" @click="open">
+		<image :src="item.avatar"
 		style="width: 100rpx;height: 100rpx;"
 		class="rounded-circle mr-2"></image>
-		<view class="flex flex-column flex-1" style="flex:1;">
+		<view class="flex flex-column flex-1">
 			<text class="font-md text-dark">{{item.username}}</text>
-			<uni-badge :text="item.age" type="error">
+			<uni-badge :text="item.age" 
+			:type="item.sex===1?'error':'primary'" 
+			size="small">
+				<text v-if="item.sex > 0" 
+				class="iconfont text-white font-sm" 
+				:class="item.sex===1?'icon-nv':'icon-nan'"
+				style="margin-right: 5rpx;"></text>
 			</uni-badge>
-		</view>
-		<view class="uni-icon uni-icon-checkbox-filled text-light-muted" :class="item.isFollow ? 'text-light-muted':'text-main'"></view>
+		</view> 
+		<view class="uni-icon uni-icon-checkbox-filled" 
+		:class="item.isFollow ? 'text-light-muted':'text-main'"></view> 
 	</view>
 </template>
 
 <script>
-	import uniBadge from '@/components/uni-ui/uni-badge/uni-badge.vue'
+	import uniBadge from '@/components/uni-ui/uni-badge/uni-badge.vue';
 	export default {
-		components:{
+		components: {
 			uniBadge
 		},
 		props:{
 			item:Object,
 			index:Number
-		}
+		},
+		methods: {
+			open() {
+				uni.navigateTo({
+					url: '/pages/user-space/user-space?user_id='+this.item.id,
+				});
+			}
+		},
 	}
 </script>
 
